427cb0ab15eb41704a98be53a5d2888344a7d966
paradox4994 <ryanpereira499@gmail.com>
2023-12-20 12:47:16 -0500

    Added routes and models

---
+++ TODO/server/routes/task.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const Task = require("../models/task");
+const task = require("../models/task");
+
+router.post("/", async (req, res) => {
+  try {
+    const task = await new Task(req.body).save();
+    res.send(task);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const task = await Task.find();
+    res.send(task);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate({ _id: req.params.id }, req.body);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    res.send(task);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+module.exports = router;
