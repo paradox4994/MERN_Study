3ba6f5a4864b473c05ce8f873a69665bc0564d15
paradox4994 <ryanpereira499@gmail.com>
2023-12-20 12:49:37 -0500

    added routes to index

--- TODO/server/index.js
+++ TODO/server/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const connection = require("./db");
 const app = express();
+const tasks = require("./routes/task");
 
 connection();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/tasks", tasks);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
